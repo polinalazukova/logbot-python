97a5db73ba9c3f35fb2ce206cae8621a176a00d2
polinalazukova <147844074+polinalazukova@users.noreply.github.com>
2024-11-26 23:57:15 +0500

    создание сервера и его проверка

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+import requests
 
 # Инициализация бота с токеном
 bot = telebot.TeleBot('7643285854:AAEu6YoNuEBt_yfK6tskNXYrYt3miHLiIXY')
@@ -6,7 +7,8 @@ bot = telebot.TeleBot('7643285854:AAEu6YoNuEBt_yfK6tskNXYrYt3miHLiIXY')
 # Список команд с описанием
 commands = [
     telebot.types.BotCommand("/start", "Get a welcome message"),
-    telebot.types.BotCommand("/help", "Info how to use this bot")
+    telebot.types.BotCommand("/help", "Info how to use this bot"),
+    telebot.types.BotCommand("/checkserver", "Check server")
 ]
 
 # Устанавливаем команды для бота
@@ -26,6 +28,17 @@ def send_help(message):
         "/start - Начать разговор и сбросить настройки.\n"
         "/help - Получить информацию о командах."
     )
+
+@bot.message_handler(commands=['checkserver'])
+def check_server(message):
+    try:
+        response = requests.get('http://localhost:5000/ping')
+        if response.status_code == 200:
+            bot.send_message(message.chat.id, f"Сервер работает: {response.json()['message']}")
+        else:
+            bot.send_message(message.chat.id, "Не удалось подключиться к серверу.")
+    except Exception as e:
+        bot.send_message(message.chat.id, f"Ошибка при подключении к серверу: {e}")
 
 @bot.message_handler(func=lambda message: True)
 def handle_unknown(message):
